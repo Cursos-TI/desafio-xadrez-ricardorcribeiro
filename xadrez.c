206934827e23dde8e5e5945e0dcc48795f5bcd80
ricardorcribeiro <ricardorcribeiro@gmail.com>
2025-03-10 20:21:52 -0300

    termino desavio nível mestre

--- xadrez.c
+++ xadrez.c
@@ -3,57 +3,75 @@
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+void movimentacaoRainha(int movimentosRainha)
+{
+    if(movimentosRainha > 0)
+    {
+        movimentacaoRainha(movimentosRainha -1);
+        printf("Esquerda %d\n", movimentosRainha);
+    }
+}
+
+void movimentacaoTorre(int movimentosTorre)
+{
+    if(movimentosTorre > 0)
+    {
+        movimentacaoTorre(movimentosTorre -1);
+        printf("Direita %d\n", movimentosTorre);
+    }
+}
+
+void movimentacaoBispo(int movimentosBispo)
+{
+    if(movimentosBispo <= 5)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            printf("Cima\n");
+            for (int z = 0; z < 1; z++)
+            {
+                printf("Direita %d\n", movimentosBispo);
+            }
+        }
+        movimentacaoBispo(movimentosBispo +1);
+    }
+}
 
 int main()
 {
-    //criando variáveis para os contadores de cada peça
-    int contadorRainha, contadorTorre, contadorBispo, contadorCavaloEsquerda, contadorCavaloBaixo;
+    int contadorCavaloDireita, contadorCavaloCima;
 
-    // utilizando for para fazendo o contador da rainha ir de 1 a 8 para imprimir as direções 8 vezes
-    for (contadorRainha = 1; contadorRainha <= 8; contadorRainha++)
-    {
-        printf("Esquerda %d\n", contadorRainha);
-    }
+    // chamando a função recursiva movimentacaoRainha para imprimir a movimentação da rainha
+    movimentacaoRainha(8);
 
     // espaçamento para melhor visualização
     printf("--------------#--------------\n");
 
-    // inicializando o contador do bispo
-    contadorBispo = 1;
-    // utilizando while para fazer o contador da torre ir de 1 a 5 para imprimir as direções 5 vezes
-    while (contadorBispo <= 5)
-    {
-        printf("Cima Direita %d\n", contadorBispo);
-        contadorBispo++;
-    }
+    // chamando a função recursiva movimentacaoTorre para imprimir a movimentação da torre
+    movimentacaoTorre(5);
 
     // espaçamento para melhor visualização
     printf("--------------#--------------\n");
 
-    // inicializando o contador da torre
-    contadorTorre = 1;
-    // utilizando do while para fazer o contador da torre ir de 1 a 5 para imprimir as direções 5 vezes
-    do
-    {
-        printf("Direita %d\n", contadorTorre);
-        contadorTorre++;
-    } while (contadorTorre <= 5);
+    // chamando a função recursiva movimentacaoBispo para imprimir a movimentação do bispo
+    movimentacaoBispo(1);
+
 
     // espaçamento para melhor visualização
     printf("--------------#--------------\n");
 
-    for (contadorCavaloEsquerda = 1; contadorCavaloEsquerda <= 1; contadorCavaloEsquerda++)
+    for (contadorCavaloDireita = 1, contadorCavaloCima = 1; contadorCavaloCima <= 2 && contadorCavaloDireita <= 2 ; contadorCavaloDireita++)
     {
-        // inicializando o contador do cavalo Baixo
-        contadorCavaloBaixo = 1;
-
-        while (contadorCavaloBaixo <= 2)
+        //loop mais interno onde faz a movimentação do cavalo para cima
+        for (contadorCavaloCima = 1; contadorCavaloCima < 3; contadorCavaloCima++)
         {
-            printf("Baixo, ");
-            contadorCavaloBaixo++;
+            //imprime a movimentação do cavalo para cima
+            printf("Cima\n");
+            //validação de o codigo deve imprimir a movimentação para a direita ou se deve passar para o proximo loop
+            if (contadorCavaloCima != 2)
+                continue;
+            printf("Direita\n");
         }
-
-        printf("Esquerda\n");
     }
 
     return 0;
